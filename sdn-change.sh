52dad94f8530f34edf2134c0abc963f16b68be82
J.R. Morgan <liveaverage@users.noreply.github.com>
2019-12-05 11:33:16 -0500

    Update sdn-change.sh

--- sdn-change.sh
+++ sdn-change.sh
@@ -1,11 +1,23 @@
 ## Confirm no VNIDs (from Master)
 oc get netnamespace
 
-## Masters
-ansible -i inventory masters -m replace -a "path=/etc/origin/master/master-config.yaml regexp='networkPluginName.*' replace='networkPluginName: "redhat/openshift-ovs-multitenant"'" --diff --check
+## Execute from a Master node
+for c in $(oc get cm -n openshift-node | awk '{print($1)}' | tail -n +2)
+do
+ oc get cm $c -o yaml --export -n openshift-node > ${c}-backup-$(date +%Y%m%d%k%M).yaml
+ oc get cm $c -o yaml --export -n openshift-node | sed 's/redhat\/openshift-ovs.*/redhat\/openshift-ovs-multitenant/g' | oc -n openshift-node replace -f -
 
-## Nodes
-ansible -i inventory nodes -m replace -a "path=/etc/origin/node/node-config.yaml regexp='networkPluginName.*' replace='networkPluginName: "redhat/openshift-ovs-multitenant"'" --diff --check
+ echo "Config map: ${c} now using networkPlugin:"
+ oc get cm $c -o yaml --export -n openshift-node | grep networkPlugin
+
+done
+
+
+## Masters (Deprecated -- use to confirm configMap update above)
+## ansible -i inventory masters -m replace -a "path=/etc/origin/master/master-config.yaml regexp='networkPluginName.*' replace='networkPluginName: "redhat/openshift-ovs-multitenant"'" --diff --check
+
+## Nodes (Deprecated -- use to confirm configMap update above)
+## ansible -i inventory nodes -m replace -a "path=/etc/origin/node/node-config.yaml regexp='networkPluginName.*' replace='networkPluginName: "redhat/openshift-ovs-multitenant"'" --diff --check
 
 ## Master Service Restarts
 ansible -i inventory masters -m shell -a "master-restart api"
